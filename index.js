14dd56fc3e860f5470081557a348357edf16cd81
Ryan Kleeberger <rylklee@gmail.com>
2017-11-11 17:26:55 -0600

    add input box on text entry

--- index.js
+++ index.js
@@ -29,5 +29,17 @@ function displayMatches(){
     document.getElementById("list").innerHTML = displayList;
 }
 
+function checkAddInput() {
+    var inputForm = `<input type="text" name="n1" oninput="checkAddInput()">`
+    document.getElementById("newInputs").innerHTML = inputForm;
+    console.log("here");
+//check if first char input
+//if yes, add another input,
+//if not, do nothing
+
+//check if input results in empty text box,
+//if yes, remove added input
+}
+
 
 

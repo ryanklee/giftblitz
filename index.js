639c432f43df9bf2cf823c013ba5cf32c6e62440
Ryan Kleeberger <rylklee@gmail.com>
2017-11-15 20:28:43 -0600

    working node/express/mongo

--- index.js
+++ index.js
@@ -1,63 +1,62 @@
-//import "jsonfile";
-//import "underscore";
+// import "jsonfile";
+const _ = require('underscore');
 
+var group = [
+  {
+    id: '',
+    name: '',
+    deadline: '',
+    msg: '',
+    url: '',
+    members: [
+      {
+        id: '',
+        name: '',
+        email: '',
+        url: '',
+        link: '',
+        match: '',
+      },
+    ],
+  },
+];
 
-var groups = [
-    {
-        id: "",
-        name: "",
-        deadline: "",
-        msg: "",
-        url: "",
-        members: [
-            {
-                id: "",
-                name: "",
-                email: "",
-                url: "",
-                link: "",
-                match: "",
-            }
-        ]
-    },
-]
-
-function hydrateGroup(){
-    group.name = document.getElementById("groupName").value;
-    group.deadline = document.getElementById("deadline").value;
-    group.msg = document.getElementById("msg").value;
-    group.url = document.getElementById("url").value;
+function hydrateGroup() {
+  group.name = document.getElementById('groupName').value;
+  group.deadline = document.getElementById('deadline').value;
+  group.msg = document.getElementById('msg').value;
+  group.url = document.getElementById('url').value;
 }
 
-function matchNames(pool){
-    matches = {};
-    var allNames = _.shuffle(pool);
-    
-    allNames.forEach(name => {
-        if (name == pool[pool.length - 1]) { pool = pool.reverse(); }
-        matches[name] = pool.pop();
-    })
-    return matches;
+function matchNames(pool) {
+  let matches = {};
+  let allNames = _.shuffle(pool);
+
+  allNames.forEach((name) => {
+    if (name == pool[pool.length - 1]) { pool = pool.reverse(); }
+    matches[name] = pool.pop();
+  });
+  return matches;
 }
 
-function readNames(){
-    var nameList = [];
-    var inputForms = document.getElementById("nameInputs");
-    for (var i = 0; i < inputForms.length; i++){
-        nameList.push(inputForms.elements[i].value);
-    }
-    return nameList;
+function readNames() {
+  let nameList = [];
+  let inputForms = document.getElementById('nameInputs');
+  for (let i = 0; i < inputForms.length; i++) {
+    nameList.push(inputForms.elements[i].value);
+  }
+  return nameList;
 }
 
-function displayMatches(){
-    var nameList = readNames();
-    var matches = matchNames(nameList);
-    var displayList = "";
+function displayMatches() {
+  let nameList = readNames();
+  let matches = matchNames(nameList);
+  let displayList = '';
 
-    for (name in matches){
-        displayList += "<p>" + name +  " gets " + matches[name] + "</p>";
-    }
-    document.getElementById("list").innerHTML = displayList;
+  for (let name in matches) {
+    displayList += '<p>' + name + ' gets ' + matches[name] + '</p>';
+  }
+  document.getElementById('list').innerHTML = displayList;
 }
 
 
--- server.js
+++ server.js
@@ -4,31 +4,32 @@ const MongoClient = require('mongodb').MongoClient
 const app = express();
 
 var db;
+var port = 3000;
 
 MongoClient.connect('mongodb://ryanklee:polecatglucosemarquessgelatin@ds259855.mlab.com:59855/giftblitz', (err, database) => {
-    if (err) return console.log(err);
+  if (err) return console.log(err);
     db = database;
-    app.listen(3000, ( ) => {
-        console.log('listening on 3000');
-    })
+    app.listen(port, ( ) => {
+      console.log('listening on 3000');
+  })
 })
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
     
-    db.collection('groups').find().toArray(function(err, results) {
-        console.log(results);
-    })
+  db.collection('groups').find().toArray(function(err, results) {
+      console.log(results);
+  })
 })
 
 app.post('/groups', (req, res) => {
-    db.collection('groups').save(req.body, (err, result) => {
-        if (err) return console.log(err);
+  db.collection('groups').save(req.body, (err, result) => {
+    if (err) return console.log(err);
 
-        console.log('saved to database');
-        res.redirect('/');
+    console.log('saved to database');
+      res.redirect('/');
     })
 })

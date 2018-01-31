38494f54334c3caa8b200eab1a2bc561b69997af
Ryan Kleeberger <rylklee@gmail.com>
2018-01-31 12:10:06 -0600

    added member and group classes

---
+++ Giftblitz/Models/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Giftblitz.Models
+{
+    public class Group
+    {
+        public string Name { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Message { get; set; }
+        public List<Member> Members { get; set; }
+    }
+}
---
+++ Giftblitz/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Giftblitz.Models
+{
+    public class Member
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Group Group { get; set; }
+        public Member Assignment { get; set; }
+        public Member Assignee { get; set; }
+        public string Email { get; set; }
+    }
+}

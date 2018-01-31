fb50c74f13315a89ddf96a0198b3259fa5731821
Ryan Kleeberger <rylklee@gmail.com>
2018-01-31 12:49:43 -0600

    added repository interface

---
+++ Giftblitz/Models/IGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Giftblitz.Models
+{
+    public interface IGroupRepository
+    {
+        IQueryable<Group> Groups { get;  }
+        IQueryable<Member> Members { get; }
+    }
+}

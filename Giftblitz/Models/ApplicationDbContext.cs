4dc1ce943b20259ba0885e383b66d48fad357067
Ryan Kleeberger <rylklee@gmail.com>
2018-01-31 19:49:49 -0600

    added dbcontext

---
+++ Giftblitz/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Giftblitz.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Member> Members { get; set; }
+    }
+}

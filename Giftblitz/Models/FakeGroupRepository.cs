cc4b93b1714712142893ac3e03f4e99112d074f6
Ryan Kleeberger <rylklee@gmail.com>
2018-01-31 19:44:43 -0600

    added members to test repo, added EF core

--- Giftblitz/Models/FakeGroupRepository.cs
+++ Giftblitz/Models/FakeGroupRepository.cs
@@ -7,20 +7,40 @@ namespace Giftblitz.Models
 {
     public class FakeGroupRepository : IGroupRepository
     {
-        DateTime now = DateTime.Now;
-
         public IQueryable<Group> Groups => new List<Group>
         {
             new Group {
                 GroupID = "testGroup1",
                 Name = "Acme",
-                Deadline = now,
+                Deadline = DateTime.Now,
                 Message = "testMessageA" },
             new Group {
                 GroupID = "testGroup2",
                 Name = "ABC Group",
-                Deadline = now,
+                Deadline = DateTime.Now,
                 Message = "testMessageB" }
         }.AsQueryable<Group>();
+
+        public IQueryable<Member> Members => new List<Member>
+        {
+            new Member
+            {
+                MemberID = "testMember1",
+                FirstName = "Joe",
+                LastName = "Jackson",
+                Email = "joej@gmail.com",
+                Assignee = "testAssignee1",
+                Assignment = "testAssignment1"
+            },
+            new Member
+            {
+                MemberID = "testMember2",
+                FirstName = "Boe",
+                LastName = "Backson",
+                Email = "Boeb@gmail.com",
+                Assignee = "testAssignee2",
+                Assignment = "testAssignment2"
+            }
+        }.AsQueryable<Member>();
     }
 }
--- Giftblitz/Models/IGroupRepository.cs
+++ Giftblitz/Models/IGroupRepository.cs
@@ -5,5 +5,6 @@ namespace Giftblitz.Models
     public interface IGroupRepository
     {
         IQueryable<Group> Groups { get;  }
+        IQueryable<Member> Members { get;  }
     }
 }

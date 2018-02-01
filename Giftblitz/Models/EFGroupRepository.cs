746b34a5e960824fc6e28ef46f41d0fd41850a4c
Ryan Kleeberger <rylklee@gmail.com>
2018-02-01 08:28:11 -0600

    implemented ef repo

---
+++ Giftblitz/Models/EFGroupRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Giftblitz.Models
+{
+    public class EFGroupRepository : IGroupRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFGroupRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Group> Groups => context.Groups;
+        public IQueryable<Member> Members => context.Members;
+    }
+}
